557b90129ded86a991222aed9fb19a36bfa20e4d
kenny <s1710931008@gms.nutc.edu.tw>
2023-06-30 05:52:47 +0800

    Home 面板組件封裝

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,11 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 測試面板組件 -->
+  <HomePanel title="新鮮好物" sub-title="新鮮好物 好多商品">
+    <div>新鮮好物的插件</div>
+  </HomePanel>
+  <HomePanel title="人氣推薦" sub-title="人氣推薦 好多商品">
+    <div>人氣推薦的插件</div>
+  </HomePanel>
 </template>
